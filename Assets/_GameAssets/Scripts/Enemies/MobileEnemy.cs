bf3fd4046708905f1fd818309891081185d41c92
mariajciudad <83364287+mariajciudad@users.noreply.github.com>
2021-05-23 21:54:01 +0200

    23/05/21

--- Assets/_GameAssets/Scripts/Enemies/MobileEnemy.cs
+++ Assets/_GameAssets/Scripts/Enemies/MobileEnemy.cs
@@ -10,7 +10,7 @@ public abstract class MobileEnemy : Enemy
     public float minAngle;
     [Range(0, 360)]
     public float maxAngle;
-    [Range(1,5)]
+    [Range(1, 5)]
     public float distanceToExplosion;
     private void Start()
     {
@@ -33,23 +33,30 @@ public abstract class MobileEnemy : Enemy
         if (distanceToPlayer <= distanceToExplosion)
         {
             Instantiate(prefabPSDeath, transform.position, transform.rotation);
-            Destroy(gameObject);
+            //Alternativa triste
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            Destroy(this);
+            //Alternativa piadosa
+            //Destroy(gameObject);
         }
     }
 
     public virtual void Rotate()
     {
-        int determinante = Random.Range(0, 100);//Número entero entre 0 y 100
-        int signo = determinante > 50 ? 1 : -1;//Expresión ternaria, tiene el mismo significado que el código siguiente
+        int determinante = Random.Range(0, 100);
+        int signo = determinante > 50 ? 1 : -1;
         /*
-        if (determinante > 50)
-        {
-            signo = 1;
-        } else
-        {
-            signo = -1;
-        }
-        */
+         * La linea de arriba es una expresion ternaria. El interrogante es una pregunta. Esto es como decir: 
+         * ¿es el valor de determinante mayor que 50? Si es sí, seria 1. Si es no, seria -1
+         * Tambien se puede escribir así:
+         if (determinante > 50)
+         {
+           signo = 1;
+         } else
+         {
+           signo = -1;
+         }
+         */
         transform.Rotate(0, Random.Range(minAngle, maxAngle) * signo, 0);
     }
 }
