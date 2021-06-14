b4cebd15f1d4b547b506603af6a653141f026c62
mariajciudad <83364287+mariajciudad@users.noreply.github.com>
2021-06-14 10:07:56 +0200

    14/6/21

---
+++ Assets/PRUEBAS-------------/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager scoreManagerInstance;
+
+    [SerializeField]
+    int score;
+
+
+    private void Awake()
+    {
+        if(scoreManagerInstance == null)
+        {
+            scoreManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);        
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if(level == 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PRUEBAS-------------/TestLoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestLoadScene : MonoBehaviour
+{
+    [SerializeField]
+    int sceneToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ChangeSceneMethod(sceneToLoad);
+        }
+    }
+
+    void ChangeSceneMethod(int n)
+    {
+        SceneManager.LoadScene(n);
+    }
+}
